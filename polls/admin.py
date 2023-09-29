1f6f3cd43b291d9114eb1299deb097bb424cc72b
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2023-09-29 18:11:47 -0400

    asdhfskfh

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Choice
+from . models import Question
+
 
 # Register your models here.
+admin.site.register(Choice)
+admin.site.register(Question)
